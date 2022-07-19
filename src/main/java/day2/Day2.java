6997fb88ec1e14320cc07085a4daf33b25542fd4
Hiep Pham <hiepph@tuta.io>
2022-07-20 00:54:13 +0200

    Day 2 part 2

--- src/main/java/day2/Day2.java
+++ src/main/java/day2/Day2.java
@@ -17,14 +17,7 @@ public class Day2 {
         return nums;
     }
 
-    public static void main(String[] args) throws IOException {
-        List<String> lines = Files.readAllLines(
-                Paths.get("src/main/resources/inputs/2.txt")
-        );
-
-        List<Integer> nums = readInts(lines.get(0));
-
-        // part 1
+    private static void part1(List<Integer> nums) {
         nums.set(1, 12);
         nums.set(2, 2);
 
@@ -50,5 +43,58 @@ public class Day2 {
         }
 
         System.out.println(nums.get(0));
+    }
+
+    private static boolean outputProduced(List<Integer> nums, int noun, int verb) {
+        nums.set(1, noun);
+        nums.set(2, verb);
+
+        int i = 0;
+        boolean halt = false;
+        while (i < nums.size()) {
+            int opCode = nums.get(i);
+
+            switch (opCode) {
+                case 1:
+                    nums.set(nums.get(i + 3), nums.get(nums.get(i + 1)) + nums.get(nums.get(i + 2)));
+                    break;
+                case 2:
+                    nums.set(nums.get(i + 3), nums.get(nums.get(i + 1)) * nums.get(nums.get(i + 2)));
+                    break;
+                case 99:
+                    halt = true;
+                    break;
+            }
+
+            if (halt) break;
+            i += 4;
+        }
+
+        return nums.get(0) == 19690720;
+    }
+
+    private static void part2(List<Integer> nums) {
+        for (int noun = 0; noun <= 99; noun++) {
+            for (int verb = 0; verb <= 99; verb++) {
+                if (outputProduced(new ArrayList<>(nums), noun, verb))  {
+                    System.out.println(100 * noun + verb);
+                    return;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(
+                Paths.get("src/main/resources/inputs/2.txt")
+        );
+
+        List<Integer> nums = readInts(lines.get(0));
+
+        // part 1
+        part1(new ArrayList<>(nums));
+
+        // part 2
+        part2(new ArrayList<>(nums));
     }
 }
