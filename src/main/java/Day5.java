b833531127f06bb77e43cddf25fc5d67fb2f1bfc
Hiep Pham <hiepph@tuta.io>
2022-07-24 18:30:14 +0200

    Day 5 part 2 backbone

--- src/main/java/Day5.java
+++ src/main/java/Day5.java
@@ -1,8 +1,70 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+public class Day5 {
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(
+                Paths.get("src/main/resources/inputs/5.txt")
+        );
+
+        System.out.println(part1(lines.get(0)));
+        System.out.println(part2(lines.get(0)));
+    }
+
+    private static int part1(String line) {
+        List<String> inputs = Arrays.asList(line.split(","));
+
+        int i = 0;
+        int diagnosticCode = 0;
+
+        while (i < inputs.size()) {
+            Instruction instruction = new Instruction(inputs, i);
+
+            int tempDiagnosticCode = instruction.execute();
+            switch (instruction.getOpCode()) {
+                case 1:
+                case 2:
+                    i += 4;
+                    break;
+                case 3:
+                    i += 2;
+                    break;
+                case 4:
+                    // get last output provided by opCode 4
+                    diagnosticCode = tempDiagnosticCode;
+                    i += 2;
+                    break;
+                case 99:
+                    // halt
+                    return diagnosticCode;
+            }
+        }
+
+        return diagnosticCode;
+    }
+
+    private static int part2(String line) {
+        List<Integer> inputs = new ArrayList<>();
+
+        for (String numString : line.split(",")) {
+            inputs.add(Integer.parseInt(numString));
+        }
+
+        Instruction2 instr = new Instruction2(inputs);
+        try {
+            instr.execute();
+        } catch (NoOpCodeException err) {
+            System.out.println(err.getMessage());
+            return -1;
+        }
+
+        return instr.getDiagnosticCode();
+    }
+}
 
 class Instruction {
     private int opCode;
@@ -72,43 +134,93 @@ class Instruction {
     }
 }
 
-public class Day5 {
-    public static void main(String[] args) throws IOException {
-        List<String> lines = Files.readAllLines(
-                Paths.get("src/main/resources/inputs/5.txt")
-        );
+class Instruction2 {
+    private List<Integer> inputs;
+    private int instructionPointer;
+    private String instr;
+    private int inputCode = 5;
+    private int diagnosticCode;
 
-        int diagosticCode = part1(lines.get(0));
-        System.out.print(diagosticCode);
+
+    public Instruction2(List<Integer> inputs) {
+        this.inputs = inputs;
+        this.instructionPointer = 0;
     }
 
-    private static int part1(String line) {
-        List<String> inputs = Arrays.asList(line.split(","));
-
-        int i = 0;
-        int diagnosticCode = 0;
-
-        while (i < inputs.size()) {
-            Instruction instruction = new Instruction(inputs, i);
-
-            int tempDiagnosticCode = instruction.execute();
-            switch (instruction.getOpCode()) {
-                case 1:
-                case 2:
-                    i += 4;
-                    break;
-                case 3:
-                case 4:
-                    // get last output provided by opCode 4
-                    diagnosticCode = tempDiagnosticCode;
-                    i += 2;
-                    break;
-                case 99:
-                    // halt
-                    return diagnosticCode;
-            }
+    public void execute() throws NoOpCodeException {
+        this.instr = String.format("%05d", inputs.get(instructionPointer));
+        int opCode = Integer.parseInt(this.instr.substring(3));
+        switch (opCode) {
+            case 1:
+                inputs.set(inputs.get(instructionPointer + 3),
+                        firstParameter() + secondParameter());
+                instructionPointer += 4;
+                break;
+            case 2:
+                inputs.set(inputs.get(instructionPointer + 3),
+                        firstParameter() * secondParameter());
+                instructionPointer += 4;
+                break;
+            case 3:
+                inputs.set(inputs.get(instructionPointer + 1), inputCode);
+                instructionPointer += 2;
+                break;
+            case 4:
+                diagnosticCode = inputs.get(inputs.get(instructionPointer + 1));
+                instructionPointer += 2;
+                break;
+            case 5:
+                if (firstParameter() != 0) {
+                    instructionPointer = secondParameter();
+                } else {
+                    instructionPointer += 4;
+                }
+                break;
+            case 6:
+                if (firstParameter() == 0) {
+                    instructionPointer = secondParameter();
+                } else {
+                    instructionPointer += 4;
+                }
+                break;
+            case 7:
+                inputs.set(inputs.get(instructionPointer + 3),
+                        firstParameter() < secondParameter() ? 1 : 0);
+                instructionPointer += 4;
+                break;
+            case 8:
+                inputs.set(inputs.get(instructionPointer + 3),
+                        firstParameter() == secondParameter() ? 1 : 0);
+                instructionPointer += 4;
+                break;
+            case 99:
+                return;
+            default:
+                throw new NoOpCodeException(opCode);
         }
 
+        execute();
+    }
+
+    private int firstParameter() {
+        return instr.charAt(2) == '0' ?
+                inputs.get(inputs.get(instructionPointer + 1)) :
+                inputs.get(instructionPointer + 1);
+    }
+
+    private int secondParameter() {
+        return instr.charAt(1) == '0' ?
+                inputs.get(inputs.get(instructionPointer + 2)) :
+                inputs.get(instructionPointer + 2);
+    }
+
+    public int getDiagnosticCode() {
         return diagnosticCode;
+    }
+}
+
+class NoOpCodeException extends Exception {
+    public NoOpCodeException(int opCode) {
+        super(String.format("No such opcode: %d", opCode));
     }
 }
