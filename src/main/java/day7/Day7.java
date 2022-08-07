73a191e28f4567659d709580eafea4f93915014d
Hiep Pham <hiepph.2406@gmail.com>
2022-08-07 14:10:26 +0200

    Solve Day 7 Part 1

---
+++ src/main/java/day7/Day7.java
@@ -0,0 +1,200 @@
+package day7;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.lang.Integer.MIN_VALUE;
+
+public class Day7 {
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(
+                Paths.get("src/main/resources/inputs/7.txt")
+        );
+
+        System.out.println(part1(readInts(lines.get(0))));
+    }
+
+    private static int part1(List<Integer> inputs) {
+        int maxThrusterSignal = MIN_VALUE;
+        List<Integer> sequence = IntStream.range(0, 5).boxed().collect(Collectors.toList());
+        Set<List<Integer>> permutations = generatePermutations(sequence);
+
+        for (List<Integer> phaseSettingSequence : permutations) {
+            Series series = new Series(phaseSettingSequence);
+
+            int thrusterSignal = series.execute(inputs);
+            if (thrusterSignal > maxThrusterSignal) {
+                maxThrusterSignal = thrusterSignal;
+            }
+        }
+
+        return maxThrusterSignal;
+    }
+
+    /**
+     * Generates a set of all permutations of a sequence.
+     */
+    private static Set<List<Integer>> generatePermutations(List<Integer> sequence) {
+        Set<List<Integer>> permutations = new HashSet<>();
+        if (sequence.size() == 0) {
+            permutations.add(sequence);
+            return permutations;
+        }
+
+        int first = sequence.get(0);
+        Set<List<Integer>> restPermutation = generatePermutations(sequence.subList(1, sequence.size()));
+
+        for (List<Integer> rest : restPermutation) {
+            for (int i = 0; i <= rest.size(); i++)  {
+                List<Integer> builder = new ArrayList<>(rest.subList(0, i));
+                builder.add(first);
+                builder.addAll(rest.subList(i, rest.size()));
+
+                permutations.add(builder);
+            }
+        }
+
+        return permutations;
+    }
+
+    private static List<Integer> readInts(String line) {
+        List<Integer> inputs = new ArrayList<>();
+
+        for (String numString : line.split(",")) {
+            inputs.add(Integer.parseInt(numString));
+        }
+
+        return inputs;
+    }
+}
+
+class Series {
+    private final List<Integer> phaseSettingSequence;
+
+    Series(List<Integer> phaseSettingSequence ) {
+        this.phaseSettingSequence = phaseSettingSequence;
+    }
+
+    /**
+     * Returns the thruster signal as the final output signal after a series of amplifiers.
+     */
+    public int execute(List<Integer> inputs) {
+        int outputSignal = 0;
+        for (int phaseSetting : phaseSettingSequence) {
+            Amplifier amp = new Amplifier(new ArrayList<>(inputs), Arrays.asList(phaseSetting, outputSignal));
+
+            try {
+                amp.execute();
+            } catch (NoOpCodeException err) {
+                System.out.println(err.getMessage());
+                return -1;
+            }
+
+            outputSignal = amp.getDiagnosticCode();
+        }
+
+        return outputSignal;
+    }
+}
+
+class Amplifier {
+    private final List<Integer> inputs;
+    private int instructionPointer;
+    private String instr;
+    private final List<Integer> inputCodes;
+    private int inputCodePointer;
+    private int diagnosticCode;
+
+
+    public Amplifier(List<Integer> inputs, List<Integer> inputCodes) {
+        this.inputs = inputs;
+        this.instructionPointer = 0;
+
+        this.inputCodes = inputCodes;
+        this.inputCodePointer = 0;
+    }
+
+    public void execute() throws NoOpCodeException {
+        this.instr = String.format("%05d", inputs.get(instructionPointer));
+        int opCode = Integer.parseInt(this.instr.substring(3));
+        switch (opCode) {
+            case 1:
+                inputs.set(inputs.get(instructionPointer + 3),
+                        firstParameter() + secondParameter());
+                instructionPointer += 4;
+                break;
+            case 2:
+                inputs.set(inputs.get(instructionPointer + 3),
+                        firstParameter() * secondParameter());
+                instructionPointer += 4;
+                break;
+            case 3:
+                // input
+                inputs.set(inputs.get(instructionPointer + 1), inputCodes.get(inputCodePointer));
+                inputCodePointer++;
+                instructionPointer += 2;
+                break;
+            case 4:
+                // output
+                diagnosticCode = inputs.get(inputs.get(instructionPointer + 1));
+                instructionPointer += 2;
+                break;
+            case 5:
+                if (firstParameter() != 0) {
+                    instructionPointer = secondParameter();
+                } else {
+                    instructionPointer += 3;
+                }
+                break;
+            case 6:
+                if (firstParameter() == 0) {
+                    instructionPointer = secondParameter();
+                } else {
+                    instructionPointer += 3;
+                }
+                break;
+            case 7:
+                inputs.set(inputs.get(instructionPointer + 3),
+                        firstParameter() < secondParameter() ? 1 : 0);
+                instructionPointer += 4;
+                break;
+            case 8:
+                inputs.set(inputs.get(instructionPointer + 3),
+                        firstParameter() == secondParameter() ? 1 : 0);
+                instructionPointer += 4;
+                break;
+            case 99:
+                return;
+            default:
+                throw new NoOpCodeException(opCode);
+        }
+
+        execute();
+    }
+
+    private int firstParameter() {
+        return instr.charAt(2) == '0' ?
+                inputs.get(inputs.get(instructionPointer + 1)) :
+                inputs.get(instructionPointer + 1);
+    }
+
+    private int secondParameter() {
+        return instr.charAt(1) == '0' ?
+                inputs.get(inputs.get(instructionPointer + 2)) :
+                inputs.get(instructionPointer + 2);
+    }
+
+    public int getDiagnosticCode() {
+        return diagnosticCode;
+    }
+}
+
+class NoOpCodeException extends Exception {
+    public NoOpCodeException(int opCode) {
+        super(String.format("No such opcode: %d", opCode));
+    }
+}
