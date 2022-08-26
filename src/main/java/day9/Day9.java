5ae333c8ea1161436798b0a50a7d5162f92f8886
Hiep Pham <hiepph@tuta.io>
2022-08-26 20:44:30 +0200

    Day 9: refactor the IntCodeComputer to handle long output

---
+++ src/main/java/day9/Day9.java
@@ -0,0 +1,157 @@
+package day9;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day9 {
+    public static void main(String[] args) throws IOException {
+        System.out.println(part1("src/main/resources/inputs/9.txt", 1));
+    }
+
+    public static long part1(String inputFilename, int inputCode) throws IOException {
+        List<String> lines = Files.readAllLines(
+                Paths.get(inputFilename)
+        );
+        List<Long> inputs = readNumbers(lines.get(0));
+
+        IntCodeComputer computer = new IntCodeComputer(inputs);
+        try {
+            computer.execute(inputCode);
+        } catch (NoOpCodeException err) {
+            System.out.println(err.getMessage());
+            return -1;
+        }
+
+        return computer.getDiagnosticCode();
+    }
+
+    private static List<Long> readNumbers(String line) {
+        List<Long> inputs = new ArrayList<>();
+
+        for (String numString : line.split(",")) {
+            inputs.add(Long.parseLong(numString));
+        }
+
+        return inputs;
+    }
+}
+
+/*
+ * Instruction has the form: ABCDE, in which:
+ *   + DE: 2-digit opcode
+ *   + C: mode of first parameter
+ *   + B: mode of second parameter
+ *   + A: mode of third parameter
+ *
+ * Types of mode:
+ *   + 0: position mode
+ *   + 1: immediate mode
+ *   + 2: relative mode
+ *
+ * ref:
+ *   + https://adventofcode.com/2019/day/2
+ *   + https://adventofcode.com/2019/day/5
+ */
+class IntCodeComputer {
+    private final List<Long> memory;
+    private long instructionPointer;
+    private String instr;
+    private List<Long> inputCodes;
+    private long diagnosticCode;
+
+    public IntCodeComputer(List<Long> inputs) {
+        this.memory = inputs;
+        this.instructionPointer = 0;
+    }
+
+    public void execute(int inputCode) throws day9.NoOpCodeException {
+        instr = String.format("%05d", getMemory(instructionPointer));
+        int opCode = (int) Long.parseLong(instr.substring(3));
+        switch (opCode) {
+            case 1:
+                setMemory(getMemory(instructionPointer + 3),
+                        getParameter(1) + getParameter(2));
+                instructionPointer += 4;
+                break;
+            case 2:
+                setMemory(getMemory(instructionPointer + 3),
+                        getParameter(1) * getParameter(2));
+                instructionPointer += 4;
+                break;
+            case 3:
+                setMemory(getMemory(instructionPointer + 1), inputCode);
+                instructionPointer += 2;
+                break;
+            case 4:
+                diagnosticCode = getParameter(1);
+                instructionPointer += 2;
+                break;
+            case 5:
+                if (getParameter(1) != 0) {
+                    instructionPointer = getParameter(2);
+                } else {
+                    instructionPointer += 3;
+                }
+                break;
+            case 6:
+                if (getParameter(1) == 0) {
+                    instructionPointer = getParameter(2);
+                } else {
+                    instructionPointer += 3;
+                }
+                break;
+            case 7:
+                setMemory(getMemory(instructionPointer + 3),
+                        getParameter(1) < getParameter(2) ? 1 : 0);
+                instructionPointer += 4;
+                break;
+            case 8:
+                setMemory(getMemory(instructionPointer + 3),
+                        getParameter(1) == getParameter(2) ? 1 : 0);
+                instructionPointer += 4;
+                break;
+            case 99:
+                return;
+            default:
+                throw new NoOpCodeException(opCode);
+        }
+
+        execute(inputCode);
+    }
+
+    private long getParameter(int ordinal) {
+        int index = 3 - ordinal;
+        char mode = instr.charAt(index);
+
+        switch (mode) {
+            case '0':
+                return getMemory(getMemory(instructionPointer + ordinal));
+            case '1':
+                return getMemory(instructionPointer + ordinal);
+//            case '2':
+            default:
+                return -1;
+        }
+    }
+
+    private long getMemory(long address) {
+        return memory.get((int) address);
+    }
+
+    private void setMemory(long index, long value) {
+        memory.set((int) index, value);
+    }
+
+    public long getDiagnosticCode() {
+        return diagnosticCode;
+    }
+}
+
+class NoOpCodeException extends Exception {
+    public NoOpCodeException(int opCode) {
+        super(String.format("No such opcode: %d", opCode));
+    }
+}
---
+++ src/test/java/Day9Test.java
@@ -0,0 +1,19 @@
+import day9.Day9;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day9Test {
+    @Test
+    void testPart1() throws IOException {
+        // make sure it also works as an Intcode computer
+        assertEquals(5821753, Day9.part1("src/main/resources/inputs/5.txt", 1));
+        assertEquals(11956381, Day9.part1("src/main/resources/inputs/5.txt", 5));
+
+        // able to return large numbers
+        assertEquals(16, Long.toString(Day9.part1("src/main/resources/tests/9.2.txt", 1)).length());
+        assertEquals(1125899906842624L, Day9.part1("src/main/resources/tests/9.3.txt", 1));
+    }
+}
