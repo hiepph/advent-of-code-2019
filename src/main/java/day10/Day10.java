345dbe0c8592060a9874ead5444ac4585cf2c81c
Hiep Pham <hiepph@tuta.io>
2022-09-16 19:06:18 +0200

    day10: strategy for part 2

--- src/main/java/day10/Day10.java
+++ src/main/java/day10/Day10.java
@@ -3,10 +3,7 @@ package day10;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Day10 {
     public static void main(String[] args) throws IOException {
@@ -29,11 +26,23 @@ public class Day10 {
 
     public static List<Tuple<Integer, Integer>> part2(
             String inputFilename,
-            Tuple<Integer, Integer> location) throws IOException {
+            Tuple<Integer, Integer> source) throws IOException {
         List<String> lines = Files.readAllLines(
                 Paths.get(inputFilename)
         );
 
+        // calculate a list of points
+        //
+        // -> sorted hashmap (by angle)
+        //          angle: <sorted set> (by distance)
+        //
+        // while len(hash map) > 0
+        //  loop sorted angles
+        //    - remove the nearest point from set
+        //    - if angle set is empty -> remove angle
+        List<Point> points = getAllPoints(lines, source);
+        SortedMap<Float, SortedSet<Point>> asteroids = buildStrategy(points);
+        return vaporise(asteroids);
     }
 
     private static List<Tuple<Integer, Integer>> getAllCoordinates(List<String> lines) {
@@ -78,5 +87,61 @@ public class Day10 {
 
         return new Tuple<>(bestLocation, maxNumAsteroids);
     }
+
+    private static List<Point> getAllPoints(List<String> lines, Tuple<Integer, Integer> source) {
+        List<Point> points = new ArrayList<>();
+
+        for (int y = 0; y < lines.size(); y++) {
+            for (int x = 0; x < lines.get(y).length(); x++) {
+                if (lines.get(y).charAt(x) == '#')
+                    points.add(new Point(x - source.first(), y - source.second()));
+            }
+        }
+
+        return points;
+    }
+
+    // Build strategy for vaporising the asteroids.
+    // Create a sorted hash map with:
+    //   + Keys are angles, sorted incrementally
+    //   + Values are sorted set, elements are points which are sorted by distance incrementally
+    private static SortedMap<Float, SortedSet<Point>> buildStrategy(List<Point> points) {
+        SortedMap<Float, SortedSet<Point>> asteroids = new TreeMap<>();
+
+        for (Point point : points) {
+            SortedSet<Point> set = asteroids.getOrDefault(point.getAngle(), new TreeSet<>());
+            set.add(point);
+
+            asteroids.put(point.getAngle(), set);
+        }
+
+        return asteroids;
+    }
+
 }
 
+class Point implements Comparable<Point> {
+    private float angle; // in radian
+    private long distance;
+
+    Point(int relative_x, int relative_y) {
+        // we're moving clockwise, hence calculate the angle using atan2(x, y)
+        angle = (float) Math.atan2(relative_x, relative_y);
+        // map angle to 0..2PI instead of -PI..PI
+        angle = (angle >= 0) ? angle : (float) (2 * Math.PI + angle);
+
+        distance = relative_y * relative_y + relative_x * relative_x;
+    }
+
+    public float getAngle() {
+        return angle;
+    }
+
+    @Override
+    public int compareTo(Point other) {
+        if (angle < other.angle || (angle == other.angle && distance < other.distance))
+            return -1;
+
+        return (angle == other.angle && distance == other.distance) ? 0 : 1;
+    }
+}
